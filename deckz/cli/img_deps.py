5dfaf63a000518ebd75a6b727aceda8098ff4e4a
m09 <142691+m09@users.noreply.github.com>
2021-05-06 15:46:51 +0200

    Add image deps command
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

---
+++ deckz/cli/img_deps.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from re import compile as re_compile
+
+from deckz.cli import app
+from deckz.paths import GlobalPaths
+
+
+@app.command()
+def img_deps(
+    image: str,
+    path: Path = Path("."),
+) -> None:
+    """Give information about shared modules usage."""
+    paths = GlobalPaths.from_defaults(path)
+    pattern = re_compile(fr'(\\V{{\[?"{image}".*\]? \| image}})')
+    shared_latex_dir = paths.shared_latex_dir
+    for f in shared_latex_dir.rglob("*.tex"):
+        if pattern.search(f.read_text(encoding="utf8")):
+            print(f.relative_to(shared_latex_dir))
