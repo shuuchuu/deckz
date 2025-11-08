c34d6193db9192f15a8cbb8ce1f5eb353f0a5bdc
mog <mog@noreply.git.ogion.eu>
2025-11-08 10:08:33 +0100

    Bump version to 24.3.0

--- src/deckz/__init__.py
+++ src/deckz/__init__.py
@@ -2,4 +2,4 @@ from .components.assets_builder import register_plot as register_plot
 from .components.assets_builder import register_plotly as register_plotly
 
 app_name = "deckz"
-__version__ = "24.2.0"
+__version__ = "24.3.0"
